193e903a94706e91dfc30ec05de7822415eab1f6
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2020-01-30 15:54:25 +0100

    the map and clone: map each child and add new props to them using clone

--- src/exercises/02.js
+++ src/exercises/02.js
@@ -16,10 +16,12 @@ class Toggle extends React.Component {
   //    be able to accept `on`, `toggle`, and `children` as props.
   //    Note that they will _not_ have access to Toggle instance properties
   //    like `this.state.on` or `this.toggle`.
-  static On = props => props.on ? props.children : null
-  static Off = props => props.on ? null : props.children
+  static On = props => (props.on ? props.children : null)
+  static Off = props => (props.on ? null : props.children)
 
-  static Button = props => <div>{props.children}</div>
+  static Button = ({on, toggle}) => (
+    <Switch on={on} onClick={this.toggle} />
+  )
 
   state = {on: false}
   toggle = () =>
@@ -39,8 +41,14 @@ class Toggle extends React.Component {
     // 2. React.cloneElement: https://reactjs.org/docs/react-api.html#cloneelement
     //
     // 🐨 you'll want to completely replace the code below with the above logic.
-    const {on} = this.state
-    return <Switch on={on} onClick={this.toggle} />
+    // const {on} = this.state
+    // return <Switch on={on} onClick={this.toggle} />
+    return React.Children.map(this.props.children, childElement => {
+      return React.cloneElement(childElement, {
+        on: this.state.on,
+        toggle: this.toggle,
+      })
+    })
   }
 }
 
